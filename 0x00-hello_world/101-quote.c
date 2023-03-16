93d15a2f1311a14c5daa7765f93df16ec1ba3187
SiMoG97 <echaaranimohamed@gmail.com>
2023-03-16 16:41:52 -0700

    fixing warning in 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char str[80] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n",  str);
 	return (1);
 }
