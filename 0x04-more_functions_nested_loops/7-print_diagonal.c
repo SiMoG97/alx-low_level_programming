60607bdbd7d520e297c82a91c9e54b1d111f328f
SiMoG97 <echaaranimohamed@gmail.com>
2023-03-23 11:35:39 -0700

    adding 7-print_diagonal.c file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints /
+ * @n: number of diagonals
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			if (j == i)
+			{
+				_putchar('\\');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
