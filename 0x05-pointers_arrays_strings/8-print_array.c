d2639d14b4054f420f2357468d887040c613da85
SiMoG97 <echaaranimohamed@gmail.com>
2023-03-27 10:53:46 -0700

    adding 8-print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_array - Entry point
+ * @a: array to print
+ * @n: number of elements to print
+ * Return: Nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+	}
+}
