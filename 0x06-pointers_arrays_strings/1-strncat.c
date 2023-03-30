366643e9d82f656020c7a5a10ad033ef59527fc2
SiMoG97 <echaaranimohamed@gmail.com>
2023-03-30 02:10:44 -0700

    adding 1-strncat.c file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+/**
+ * _strncat - Concatenates two strings
+ * @dest: string 1
+ * @src: string 2
+ * @n: number of bytes
+ * Return: returns a pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dLen, i;
+
+	for (dLen = 0; dest[dLen] != '\0'; dLen++)
+	{}
+
+	for (i = 0; i < n; i++)
+	{
+		dest[dLen + i] = src[i];
+	}
+	return (dest);
+}
