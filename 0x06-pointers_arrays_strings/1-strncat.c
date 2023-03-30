6a72864413e33fd2059afca545ee049e639354ea
SiMoG97 <echaaranimohamed@gmail.com>
2023-03-30 02:24:28 -0700

    fixing error maybe

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,9 +13,10 @@ char *_strncat(char *dest, char *src, int n)
 	for (dLen = 0; dest[dLen] != '\0'; dLen++)
 	{}
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[dLen + i] = src[i];
 	}
+	dest[dLen + i] = '\0';
 	return (dest);
 }
