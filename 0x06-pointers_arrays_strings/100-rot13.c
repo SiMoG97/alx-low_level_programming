e208e4d7d62f978e3c4b558d6b333f5fb2e61534
SiMoG97 <echaaranimohamed@gmail.com>
2023-03-31 18:10:42 -0700

    adding 100-rot13.c file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+/**
+ * rot13 - a function that encodes a string using rot13.
+ * @s: string
+ * Return: encoded string pointer
+ */
+
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for ( ; (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'); )
+		{
+			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+			{
+				s[i] += 13;
+			}
+			else
+			{
+				s[i] -= 13;
+			}
+			break;
+		}
+	}
+	return (s);
+}
