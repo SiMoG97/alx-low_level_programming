6c533e900fa88db9dc4830ac450fc7fa07800af7
SiMoG97 <echaaranimohamed@gmail.com>
2023-04-11 17:16:41 -0700

    fixing error again

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 /**
  * str_concat - Concatenates two strings
  * @s1: string 1
@@ -8,41 +9,25 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int len1 = 1, len2 = 1, i;
+	unsigned int len1, len2;
 	char *concat;
 
-	if (s1 != NULL)
-	{
-		for (len1 = 0; s1[len1] != '\0'; len1++)
-			continue;
-	}
-	if (s2 != NULL)
-	{
-		for (len2 = 0; s2[len2] != '\0'; len2++)
-			continue;
-	}
-	concat = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	concat = (char *)malloc(len1 + len2 + 1);
 
 	if (concat == NULL)
 		return (NULL);
 
-	if (s1 != NULL)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-			concat[i] = s1[i];
-		}
-	}
 
-	concat[i] = '\0';
-	i = 0;
-	if (s2 != NULL)
-	{
-		for (; s2[i] != '\0'; i++)
-		{
-			concat[len1 + i] = s2[i];
-		}
-	}
-	concat[len1 + i] = '\0';
+	strcpy(concat, s1);
+	strcat(concat, s2);
 	return (concat);
+
 }
