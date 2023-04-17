1285705259844f9ec8afd2bf775c95e5241d7307
SiMoG97 <echaaranimohamed@gmail.com>
2023-04-17 02:21:41 -0700

    adding files

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+/**
+ * init_dog - Initialize a variable of type struct dog
+ * @d: the variable
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: the owner of the dog
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
