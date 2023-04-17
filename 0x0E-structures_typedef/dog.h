c079c5977febfb7ae1f905ae8d33a5d111f67703
SiMoG97 <echaaranimohamed@gmail.com>
2023-04-17 02:38:39 -0700

    fixing errors again and again

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,7 +9,7 @@
   *
   * Description: dog's attributes
   */
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
@@ -17,10 +17,12 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for dog
+ * dog_t - Typedef for dog struct
  */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
