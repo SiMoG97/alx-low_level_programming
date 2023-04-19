13e73e6559c97aa5947a715078395f107cf0283b
SiMoG97 <echaaranimohamed@gmail.com>
2023-04-19 02:34:50 -0700

    fixing errors in these files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * print_name - a function that prints a name
  * @name: a name to print
@@ -7,5 +8,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * int_index - a function that searches for an integer
  * @array: the array to look in
@@ -9,6 +10,9 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
 
 	if (size <= 0)
 		return (-1);
