4e4026b1656fb53eec24cd02b4664ceeea8884b6
SiMoG97 <echaaranimohamed@gmail.com>
2023-04-27 10:40:13 -0700

    fixing betty agian

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * list_len -a function that returns the number of elements in a linked list 
+ * list_len -a function that returns the number of elements in a linked list
  * @h: a pointer to the linked list
- * Return: the lenght of a linked list 
+ * Return: the lenght of a linked list
  */
 
 size_t list_len(const list_t *h)
