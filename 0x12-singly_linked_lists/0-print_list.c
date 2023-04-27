7df92b335780a42b6c00d8ff30ec30f8953475eb
SiMoG97 <echaaranimohamed@gmail.com>
2023-04-27 09:54:27 -0700

    fixing error again

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 
 /**
  * print_list - prints all the elements of a list_t list
- * @h: list
+ * @h: pointer to the list_t list
  * Return: the number of nodes
  */
 
@@ -10,15 +10,15 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		n++;
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
+		n++;
 	}
 	return (n);
 }
