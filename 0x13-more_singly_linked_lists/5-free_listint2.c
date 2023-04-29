9be655d6e1deb3151747e28eb25622dec412ff0a
SiMoG97 <echaaranimohamed@gmail.com>
2023-04-29 07:28:09 -0700

    adding 5-free_listint2.c file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: a pointer to a pointer to the head of the list
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+
+	*head = NULL;
+}
