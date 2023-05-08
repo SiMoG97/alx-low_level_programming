f0912c15e31496ace71fe1e0e9760f4c887f1cfe
SiMoG97 <echaaranimohamed@gmail.com>
2023-05-08 15:42:58 -0700

    updating main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,6 +14,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void print_error(char *message, char *filename, int exit_code);
+void close_file(int fd, char *filename);
 
 #endif
