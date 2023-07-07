228f694d9af9f71b94418e03efde2b599e682067
SiMoG97 <echaaranimohamed@gmail.com>
2023-07-07 07:31:38 -0700

    Task-0: error handling

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
+	if (!table)
+		return (NULL);
+
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	table->array = malloc(size * sizeof(hash_node_t *));
+	
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
