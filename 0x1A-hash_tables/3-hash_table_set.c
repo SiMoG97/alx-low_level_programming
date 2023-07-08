4cb2fe14db5d96a5f6c392990e4fb4e45d9b31a0
SiMoG97 <echaaranimohamed@gmail.com>
2023-07-07 17:05:21 -0700

    Task-3: adding 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,81 @@
+#include "hash_tables.h"
+
+/**
+ * create_node - creates a new hash_node_t node
+ * @key: the key
+ * @value: the value
+ * Return: the new created node
+ */
+
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *newNode = malloc(sizeof(hash_node_t));
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->key = strdup(key);
+	if (newNode->key == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->value = strdup(value);
+	if (newNode->value == NULL)
+	{
+		free(newNode->key);
+		free(newNode);
+		return (NULL);
+	}
+	newNode->next = NULL;
+
+	return (newNode);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table
+ *
+ * @ht: the hash table to add value to it
+ * @key: the key
+ * @value: the value associated withthe key
+ *
+ * Return: Always 0 (Success)
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *curr, *newNode;
+	char *newValue;
+
+	if (!key || !value || !ht || !(ht->array) || !ht->size || strlen(key) == 0)
+		return (0);
+
+	newNode = create_node(key, value);
+	if (newNode == NULL)
+		return (0);
+
+	if (ht->array[index])
+	{
+		curr = ht->array[index];
+		while (curr)
+		{
+			if (strcmp(curr->key, key) == 0)
+			{
+				free(curr->value);
+				newValue = strdup(value);
+				if (!newValue)
+					return (0);
+				curr->value = newValue;
+				return (1);
+			}
+			curr = curr->next;
+		}
+	}
+
+	newNode->next = ht->array[index];
+	ht->array[index] = newNode;
+
+	return (1);
+}
