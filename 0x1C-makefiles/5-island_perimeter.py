ff1646c76522768212150c30653972189cd37fec
SiMoG97 <echaaranimohamed@gmail.com>
2023-08-23 14:41:20 -0700

    fixing bug in 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,16 +13,24 @@ def island_perimeter(grid):
         int: the perimeter of the island
     """
     perim = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    gridLen = len(grid)
+
+    if grid != []:
+        rowLen = len(grid[0])
+
+    for i in range(gridLen):
+        for j in range(rowLen):
             if grid[i][j] == 1:
-                if j - 1 >= 0 and grid[i][j - 1] == 0:
+                if (j - 1) == -1 or grid[i][j - 1] == 0:
                     perim += 1
-                if j + 1 < len(grid[i]) and grid[i][j + 1] == 0:
+
+                if (j + 1) == rowLen or grid[i][j + 1] == 0:
                     perim += 1
-                if i - 1 >= 0 and grid[i - 1][j] == 0:
+
+                if (i - 1) == -1 or grid[i - 1][j] == 0:
                     perim += 1
-                if i + 1 < len(grid) and grid[i + 1][j] == 0:
+
+                if (i + 1) == gridLen or grid[i + 1][j] == 0:
                     perim += 1
 
     return perim
